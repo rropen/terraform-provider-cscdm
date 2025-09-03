cb4d2f278be293b25d875e9f074887febe87b093
Your Name <lmcrean@gmail.com>
2025-09-03 09:34:47 +0100

    refactor: improve flushLoop logic with non-blocking trigger handling and graceful shutdown

--- internal/cscdm/cscdm.go
+++ internal/cscdm/cscdm.go
@@ -56,33 +56,55 @@ func (c *Client) Configure(apiKey string, apiToken string) {
 }
 
 func (c *Client) flushLoop() {
-	for {
-		triggerChan := make(chan struct{})
-		go func() {
+	// Single trigger channel used throughout lifetime
+	triggerChan := make(chan struct{}, 1)
+	// Start the trigger watcher goroutine
+	triggerStop := make(chan struct{})
+	go func() {
+		defer close(triggerChan) // Signal flushLoop to exit when we're done
+		for {
 			c.flushTrigger.L.Lock()
 			c.flushTrigger.Wait()
 			c.flushTrigger.L.Unlock()
-			close(triggerChan)
-		}()
 
+			select {
+			case <-triggerStop:
+				return
+			default:
+				// Non-blocking send - if channel full, trigger already pending
+				select {
+				case triggerChan <- struct{}{}:
+				default:
+				}
+			}
+		}
+	}()
+
+	for {
 		flushTimer := time.NewTimer(FLUSH_IDLE_DURATION)
 
 		select {
 		case <-triggerChan:
 			// Flush triggered; reset flush timer
 			flushTimer.Stop()
-			continue
+			// Drain the channel in case of multiple signals
+			select {
+			case <-triggerChan:
+			default:
+			}
 		case <-flushTimer.C:
 			// Timer expired; flush queue
 			err := c.flush()
 
 			if err != nil {
-				fmt.Fprintf(os.Stderr, "failed to flush queue: %s", err.Error())
-				return
+				fmt.Fprintf(os.Stderr, "failed to flush queue: %s\n", err.Error())
+				// Continue - don't return/terminate
 			}
 		case <-c.flushLoopStopChan:
 			// Stop flush loop
 			flushTimer.Stop()
+			close(triggerStop) // Stop the trigger watcher
+			<-triggerChan      // Wait for it to close the channel
 			return
 		}
 	}
